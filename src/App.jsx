f8bec3062706eeb729107304a82e95ffb7e040c3
praveen piramanayagam <praveennayagam18@gmail.com>
2025-03-04 11:19:48 +0530

    protected url revert

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Navbar from "./components/Navbar";
+import Nosuchpage from "./pages/Nosuchpage";
 import Auth from "./pages/Auth";
-
-// Protected Route Component
-const ProtectedRoute = ({ children }) => {
-  const user = localStorage.getItem("user"); // Check if user is logged in
-  return user ? children : <Navigate to="/" replace />;
-};
 
 function App() {
   const [cart, setCart] = useState([]);
@@ -18,29 +13,10 @@ function App() {
     <Router>
       <Navbar cart={cart} />
       <Routes>
-        {/* Public Route */}
-        <Route path="/" element={<Auth />} />
-
-        {/* Protected Routes */}
-        <Route
-          path="/home"
-          element={
-            <ProtectedRoute>
-              <Home cart={cart} setCart={setCart} />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/cart"
-          element={
-            <ProtectedRoute>
-              <Cart cart={cart} setCart={setCart} />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Redirect unknown routes to Auth */}
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/home" element={<Home cart={cart} setCart={setCart} />} />
+        <Route path="/cart" element={<Cart cart={cart} setCart={setCart} />} />
+        <Route path="/" element={<Auth/>}/>
+        <Route path="*" element={<Auth/>} />
       </Routes>
     </Router>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ const Home = ({ cart, setCart }) => {
     }
   
     try {
-      const res = await fetch("http://localhost:5000/auth/user", {
+      const res = await fetch("https://ecommerce-server-ki4x.onrender.com/auth/user", {
         credentials: "include", // Send session cookies with the request
       });
       const data = await res.json();
@@ -33,6 +33,7 @@ const Home = ({ cart, setCart }) => {
       navigate("/");
     }
   };
+  
   
   
 
